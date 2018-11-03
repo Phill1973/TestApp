a109266c62995bfd2a4b98129cad3bd48d134985
philipp_gleich <philipp_gleich@hotmail.de>
2018-11-03 14:25:43 -0400

    Add secure passwored to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_094105) do
+ActiveRecord::Schema.define(version: 2018_11_03_180119) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_03_094105) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

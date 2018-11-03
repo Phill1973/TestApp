467deab6173cc3a54d01b429824781691b2e06ef
philipp_gleich <philipp_gleich@hotmail.de>
2018-11-03 06:06:23 -0400

    add association between user and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :text, presence: true, length: { minimum: 10, maximum: 800 }
+  validates :user_id, presence: true
 end
